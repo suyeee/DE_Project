6ec542618e5bac8040019f0361faac85259f4b20
suyeee <suyeon0727@naver.com>
2022-06-04 23:40:10 +0900

    좋아요수 추출 오류 수정

--- insta_VSCode_ver.py
+++ insta_VSCode_ver.py
@@ -9,11 +9,11 @@ from selenium.webdriver.common.by import By
 def get_chrome_driver():
     # 1. 크롬 옵션 세팅
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--incognito")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-setuid-sandbox")
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--incognito")  # 시크릿모드로 접속
+    chrome_options.add_argument("--no-sandbox")  # 대부분의 리소스에 대한 액세스 방지
+    chrome_options.add_argument("--disable-setuid-sandbox")  # 크롬 드라이버에 setuid를 하지않음으로써 크롬의 충돌 방지
+    chrome_options.add_argument('--disable-dev-shm-usage')  # 메모리 부족으로 인한 에러 방지
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])  # 로그 숨기기
 
     # 2. driver 생성하기
     driver = webdriver.Chrome(
@@ -26,15 +26,24 @@ def get_chrome_driver():
 
 driver = get_chrome_driver()
 
+# 브라우저 창 한개 더 열기
+driver.execute_script('window.open("about:blank", "_blank");')
+
+# 브라우저 인덱스
+tabs = driver.window_handles
+
+# 검색어에 대한 링크 추출
 def searching_tag(word):
     url = "https://www.instagram.com/explore/tags/" + str(word)
     return url
 
+# 검색후 나온 첫번째 게시글 클릭
 def first_click(driver):
     first = driver.find_element(By.CSS_SELECTOR, '._aagw')
     first.click()
     time.sleep(5)
   
+# 게시글 내용 추출
 def get_content(driver):
     html = driver.page_source
     soup = BeautifulSoup(html, 'lxml')
@@ -61,45 +70,49 @@ def get_content(driver):
     # 5. 좋아요수는 없고 조회수만 있는 경우
     try:
         # 3번 경우(좋아요수 비공개인것들 처리)
+        # 좋아요수 비공개인것들만 실행하도록 필터링
         like_pattern = '여러'
         like = soup.select_one('div._aacl._aaco._aacw._aacx._aada._aade').text
-        re.match(like_pattern, like).group()
+        re.match(like_pattern, like).group()  # error 발생시키기(3번경우가 아닌경우 error 발생)
         
-        print('match=ok')  # 오류를 확인하기 위한 출력
+        # 링크 추출
+        link = soup.select('._aacl._aaco._aacu._aacx._aad6._aade > a')[1]['href']
+        like_link = 'https://www.instagram.com' + str(link)
         
-        # '여러 명'이 좋아합니다. 클릭
-        many = driver.find_element(By.CSS_SELECTOR, '._aacl._aaco._aacw._aacx._aada._aade')
-        print('click=error')  # 오류를 확인하기 위한 출력
-        many.click()
-        print('click=ok')  # 오류를 확인하기 위한 출력
-        time.sleep(3)
+        # 두번째 브라우저 창으로 이동
+        driver.switch_to.window(tabs[1])
+        driver.get(like_link)
+        time.sleep(5)
         
-        # '좋아요' 목록에서 유저id 추출후 좋아요수 세기
-        like = len(soup.select('span._aacl._aaco._aacw._aacx._aad7._aade').text)
-        print(like)  # 오류를 확인하기 위한 출력
+        # 두번째 브라우저의 html 코드 가져오기
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        
+        # 좋아요를 누른 유저수 추출
+        like = len(soup.select('._aacl._aaco._aacw._aacx._aada._ab58'))
         like = f'좋아요 {like}개'
-        view = 0
+        view = '조회 0회'
         
-        # 창 닫기
-        close = driver.find_element(By.CSS_SELECTOR, '._ab9y')
-        print('close=error')  # 오류를 확인하기 위한 출력
-        close.click()
-        print('close=ok')  # 오류를 확인하기 위한 출력
+        # 다시 메인 브라우저 (첫번째 브라우저로 이동)
+        driver.switch_to.window(tabs[0])
         time.sleep(3)
-
+        
     except:
         try:
             # 5번경우
-            like = 0
+            # 조회수만 추출
+            like = '좋아요 0개'
             view = soup.select('div._aacl._aaco._aacw._aacx._aad6._aade')[1].text
         except:
             try:
                 # 1,4번 경우
                 like = soup.select_one('div._aacl._aaco._aacw._aacx._aada._aade').text
-                view = 0
+                view = '조회 0회'
             except:
-                like = 0
-                view = 0
+                # 2번 경우
+                # 좋아요수, 조회수 둘다 없는 경우
+                like = '좋아요 0개'
+                view = '조회 0회'
 
     data = [date, like, view, hashtags, content]
     
@@ -112,6 +125,8 @@ def move_next(driver):
     time.sleep(3)
     
 # 크롤링 시작
+# 메인 브라우저
+driver.switch_to.window(tabs[0])
 driver.get('https://www.instagram.com')
 time.sleep(3)
 
@@ -128,7 +143,6 @@ input_pw.send_keys(password)
 input_pw.submit()
 
 time.sleep(5)
-print('login success')  # 오류를 확인하기 위한 출력
 
 # 로그인 정보 저장 묻는거
 driver.find_elements(By.CSS_SELECTOR, '.sqdOP')[1].click()
@@ -142,17 +156,14 @@ time.sleep(3)
 word = input("검색어를 입력하세요 : ")
 word = str(word)
 word_url = searching_tag(word)
-print(word_url)  # 오류를 확인하기 위한 출력
 
 # 검색 결과 페이지 열기
 driver.get(word_url)
-print('page_open')  # 오류를 확인하기 위한 출력
 time.sleep(10)
 
 
 # 첫 번째 게시물 클릭
 first_click(driver)
-print('click')  # 오류를 확인하기 위한 출력
 
 # 데이터 수집
 results = []
@@ -163,20 +174,24 @@ for i in range(target):
     try:   
         data = get_content(driver)
         print(data)  # 오류를 확인하기 위한 출력
-        print('data')  # 오류를 확인하기 위한 출력
         results.append(data)
-        print('append')  # 오류를 확인하기 위한 출력
         move_next(driver)
         print('next')  # 오류를 확인하기 위한 출력
        
     except:
+        # 게시글에 사진만있고 글 내용이 아무것도 없는경우
         print('error')  # 오류를 확인하기 위한 출력
         time.sleep(2)
         move_next(driver)
 
+# 드라이버 종료
+driver.quit()
 
+# 결과물 저장 
+# 지금은 일단 csv로 저장, 나중엔 sql로 저장
 import pandas as pd
 
 results_df = pd.DataFrame(results)
 results_df.columns = ['date', 'like', 'view', 'tag', 'content']
 results_df.to_csv('insta.csv')
+print('save : ok')  # 오류를 확인하기 위한 출력
